eff116ddf364a8a3af435911d984496c6c2f9480
박영호 <mellonia.bluerose@gmail.com>
2019-01-09 16:25:19 +0900

    메인페이지 업데이트

--- foodle/public/js/index.js
+++ foodle/public/js/index.js
@@ -1,3 +1,10 @@
+let email;
+let password; // 영문, 숫자, 특문 중 2개 선택 / 대소문자 안가림 / 8자리 이상.
+let emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+let pwNum;
+let pwEng;
+let pwSpe;
+
 // 구글 로그인
 function onSignIn(googleUser) {
     let profile = googleUser.getBasicProfile();
@@ -45,7 +52,110 @@ function signOut() {
         console.log('User signed out.');
     });
 }
+// 이메일 로그인 validation check
+function loginValidationCheck() {
+    email = $('.email').val();
+    password = $('.password').val();
+    pwNum = password.search(/[0-9]/g);
+    pwEng = password.search(/[a-z]/ig);
+    pwSpe = password.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
 
+    if(email === '') {
+        alert('이메일을 입력해주세요.');
+        return;
+    }
+    if(!emailRegExp.test(email)) {
+        alert('이메일 형식을 지켜주세요.');
+        return;
+    }
+    if(password === '') {
+        alert('비밀번호를 입력해주세요.');
+        return;
+    }
+    if(password.length < 8) {
+        alert('비밀번호는 8자리 이상이어야 합니다.');
+        return;
+    }
+    if((pwNum < 0 && pwEng < 0) || (pwNum < 0 && pwSpe < 0) || (pwEng < 0 && pwSpe < 0)) {
+        alert('비밀번호는 숫자, 영문, 특수문자 중 2가지 이상을 혼합해야 합니다.');
+        return;
+    }
+    login();
+}
+// 이메일 로그인
+function login () {
+    const info = {
+        url: "/api/v1/users/login",
+        method: "POST",
+        body: {
+            email: email,
+            password: password
+        },
+        success: function (res) {
+            console.log('loginReq success');
+            sessionStorage.setItem('login', '0');
+            sessionStorage.setItem('email', email);
+            alert('성공적으로 로그인되었습니다.');
+            window.location.replace(res.redirect);
+        },
+        error: function (e) {
+            console.log('ajax call error: login page - loginReq');
+            if (e.status === 403 &&
+                (e.responseText.includes("No doctor account found with given email address.") ||
+                    e.responseText.includes("Password wrong"))) {
+                alert("아이디/비밀번호를 확인해주세요.");
+            } else if (e.status === 404 && e.responseText.includes("API call URL not found.")) {
+                console.log("check your URL, method(GET/POST)");
+                alert("로그인에 실패했습니다.");
+            } else if (e.status === 400 && e.responseText.includes("not given")) {
+                console.log("check your parameters");
+                alert("로그인에 실패했습니다.");
+            } else if(e.status === 0){
+                if(navigator.onLine){
+                    console.log('status : 0');
+                }else {
+                    console.log('internet disconnected');
+                    window.location.reload();
+                }
+            }  else {
+                console.log(e.responseText);
+                console.log(e);
+                alert("로그인에 실패했습니다.");
+            }
+        }
+    };
+    sendTokenReq(info);
+}
+// 임시비밀번호 발급
+function reIssueValidationCheck() {
+    email = $('#signedup-email').val();
+
+    if(email === '') {
+        alert('이메일을 입력해주세요.');
+        return;
+    }
+    if(!emailRegExp.test(email)) {
+        alert('이메일 형식을 지켜주세요.');
+        return;
+    }
+    const info = {
+        url: "/api/v1/users/send_new_password",
+        method: "POST",
+        body: {
+            email: email
+        },
+        success: function (res) {
+            if (res.success) {
+                alert('이메일로 임시 비밀번호를 발급해드렸습니다!');
+            }
+        },
+        error: function (e) {
+            console.log(e);
+            alert('임시 비밀번호 발송 실패.');
+        }
+    }
+    sendTokenReq(info);
+}
 
 $(document).ready(() => {
     // 네이버 로그인 설정
@@ -56,12 +166,12 @@ $(document).ready(() => {
             callbackUrl: location.href,
             callbackHandle: true,
 			isPopup: false, /* 팝업을 통한 연동처리 여부 */
-			loginButton: {color: "green", type: 3, height: 50} /* 로그인 버튼의 타입을 지정 */
+			loginButton: {color: "green", type: 3, height: 70} /* 로그인 버튼의 타입을 지정 */
 		}
     );
 	/* 설정정보를 초기화하고 연동을 준비 */
     naverLogin.init();
-    $('#naverIdLogin_loginButton img')[0].src = '/images/naver2.png';
+    $('#naverIdLogin_loginButton img')[0].src = '/images/naver.png';
 
     let loginValue = sessionStorage.getItem('login');
     if (loginValue === '0' || loginValue === null) {
@@ -169,4 +279,14 @@ $(document).ready(() => {
     $('.google-login').click(() => {
         $('.abcRioButton').trigger('click');
     });
+
+    // 로그인 버튼 클릭
+    $('#login-btn').click(() => {
+        loginValidationCheck();
+    })
+
+    // 임시 비번 발급 클릭
+    $('#reissue-pw').click(() => {
+        reIssueValidationCheck();
+    })
 })
