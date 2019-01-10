6daed201d005cb3a229d83088e6a40a83f727cc2
박영호 <mellonia.bluerose@gmail.com>
2019-01-10 12:26:44 +0900

    회원가입 페이지 로직 및 스타일 업데이트

--- foodle/public/js/signup.js
+++ foodle/public/js/signup.js
@@ -1,146 +1,139 @@
-let allCheck, accessCheck, dataCheck, optLength = $('.email select option').length;
-let signUpStep = 1;
 let platform;
-let firstTab = $('#basic-info');
-let secondTab = $('#additional-info');
-let email, password, nickname, gender, ageGroup, nicknameCheck = false;
-let emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+let name, email, password, passwordCheck, gender, ageGroup, phone;
+const emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+const phoneRegExp = /^\d{3}-\d{3,4}-\d{4}$/; 
 let pwNum, pwEng, pwSpe;
 
-function agreeTerms(i) {
-    allCheck = $('.terms input')[0].checked;
-    accessCheck = $('.terms input')[1].checked;
-    dataCheck = $('.terms input')[2].checked;
-    if (i === 0) {
-        if(allCheck) {
-            $('.terms input')[0].checked = false;
-            $('.terms input')[1].checked = false;
-            $('.terms input')[2].checked = false;
-        } else {
-            $('.terms input')[0].checked = true;
-            $('.terms input')[1].checked = true;
-            $('.terms input')[2].checked = true;
-        }
-    } else if(i === 1){
-        location.href= "/access-terms";
-    } else if(i === 2){
-        location.href= "/data-terms";
-    }
-    autoAgree();
-}
-
 function autoAgree() {
-    if($('.terms input')[1].checked && $('.terms input')[2].checked) $('.terms input')[0].checked = true;
-    else $('.terms input')[0].checked = false;
-}
-
-function Content() {
-    for(let i = 1; i < optLength-1; i++) {
-        if($('.email select option')[i].selected){
-            $('.email input')[1].value = $('.email select option')[i].innerHTML;
-        }
-    }
-}
-
-function changeStep() {
-    if (signUpStep === 1) {
-        firstTab.removeClass('tab').addClass('tab active');
-        secondTab.removeClass('tab active').addClass('tab');
-        $('.complete-btn')[0].style.display = 'none';
-        $('.next-btn')[0].style.display = 'block';
-        $('.additional-info-form')[0].style.display = 'none';
-        $('.basic-info-form')[0].style.display = 'block';
+    if ($('#access-terms')[0].checked && $('#privacy-policy')[0].checked) {
+        $('#all-agree')[0].checked = true;
     } else {
-        secondTab.removeClass('tab').addClass('tab active');
-        firstTab.removeClass('tab active').addClass('tab');
-        $('.next-btn')[0].style.display = 'none';
-        $('.complete-btn')[0].style.display = 'block';
-        $('.additional-info-form')[0].style.display = 'block';
-        $('.basic-info-form')[0].style.display = 'none';
+        $('#all-agree')[0].checked = false;
     }
 }
 
 function selectEmailPlatform(i) {
     let email = $('.email input').val();
-    platform = $('#platformList button')[i].innerHTML;
-    console.log($('#platformList'));
-    if (i === 6) {
-        $('.email input')[0].value = '';
+    platform = $('#platform-list button')[i].innerHTML;
+
+    if (platform === '직접 입력') {
+        $('.platform')[0].disabled = false;
     } else {
-        if (email === '') {
-            $('.email input')[0].value = platform;
-        } else if (email !== '' && email.indexOf('@') !== -1) {
-            $('.email input')[0].value = `${email.split('@')[0]}${platform}`;
-        } else if (email !== '' && email.indexOf('@') === -1) {
-            $('.email input')[0].value = `${email}${platform}`;
+        $('.platform')[0].disabled = true;
+        $('.platform')[0].value = platform;
+    }
+}
+
+function passwordValidationCheck () {
+    pwNum = $('#password').val().search(/[0-9]/g);
+    pwEng = $('#password').val().search(/[a-z]/ig);
+    pwSpe = $('#password').val().search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+    
+    if ($('#password').val() !== '' && $('#password').val().length >= 8 && !((pwNum < 0 && pwEng < 0) || (pwEng < 0 && pwSpe < 0) || (pwSpe < 0 && pwNum < 0))) {
+        $('.password-complete')[0].childNodes[1].style.display = 'block';
+    } else {
+        $('.password-complete')[0].childNodes[1].style.display = 'none';
+    }
+    if ($('#password-check').val() !== '') {
+        if ($('#password').val() === $('#password-check').val()) {
+            $('.password-complete')[1].childNodes[1].style.display = 'block';
+        } else {
+            $('.password-complete')[1].childNodes[1].style.display = 'none';
         }
     }
 }
 
-function basicInfoValidationCheck() {
-    email = $('#email').val();
+function passwordEqualCheck () {
+    if ($('#password-check').val() !== '') {
+        if ($('#password').val() === $('#password-check').val()) {
+            $('.password-complete')[1].childNodes[1].style.display = 'block';
+        } else {
+            $('.password-complete')[1].childNodes[1].style.display = 'none';
+        }
+    }
+}
+
+function firstInfoValidationCheck() {
+    let firstName = $('#first-name').val();
+    let lastName = $('#last-name').val();
+    name = `${firstName}${lastName}`;
+    email = `${$('#email').val()}@${$('#platform').val()}`;
     password = $('#password').val();
-    allCheck = $('.terms input')[0].checked;
+    passwordCheck = $('#password-check').val();
     pwNum = password.search(/[0-9]/g);
     pwEng = password.search(/[a-z]/ig);
     pwSpe = password.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
 
-    if(email === '') {
-        alert('이메일을 입력해주세요.');
-        return;
-    }
-    if(!emailRegExp.test(email)) {
-        alert('이메일 형식을 지켜주세요.');
-        return;
-    }
-    if(password === '') {
-        alert('비밀번호를 입력해주세요.');
-        return;
-    }
-    if(password.length < 8) {
-        alert('비밀번호는 8자리 이상이어야 합니다.');
-        return;
-    }
-    if((pwNum < 0 && pwEng < 0) || (pwNum < 0 && pwSpe < 0) || (pwEng < 0 && pwSpe < 0)) {
-        alert('비밀번호는 숫자, 영문, 특수문자 중 2가지 이상을 혼합해야 합니다.');
-        return;
-    }
-    if(!allCheck) {
-        alert('약관에 동의해주세요.');
-        return;
-    }
-    signUpStep = 2;
-    changeStep();
+    // if (firstName === '' || firstName === null) {
+    //     alert('성을 입력해주세요.');
+    //     return;
+    // }
+    // if (lastName === '' || lastName === null) {
+    //     alert('이름을 입력해주세요.');
+    //     return;
+    // }
+    // if (email === '') {
+    //     alert('이메일을 입력해주세요.');
+    //     return;
+    // }
+    // if (gender === '' || gender == null) {
+    //     alert('성별을 선택해주세요.');
+    //     return;
+    // }
+    // if (!emailRegExp.test(email)) {
+    //     alert('이메일 형식을 지켜주세요.');
+    //     return;
+    // }
+    // if (password === '') {
+    //     alert('비밀번호를 입력해주세요.');
+    //     return;
+    // }
+    // if (password.length < 8) {
+    //     alert('비밀번호는 8자리 이상이어야 합니다.');
+    //     return;
+    // }
+    // if ((pwNum < 0 && pwEng < 0) || (pwNum < 0 && pwSpe < 0) || (pwEng < 0 && pwSpe < 0)) {
+    //     alert('비밀번호는 숫자, 영문, 특수문자 중 2가지 이상을 혼합해야 합니다.');
+    //     return;
+    // }
+    // if (password !== passwordCheck) {
+    //     alert('비밀번호가 일치하지 않습니다.');
+    //     return;
+    // }
+    $('.next-button')[0].style.display = 'none';
+    $('.signup-button')[0].style.display = 'block';
+    $('.input-container')[0].style.display = 'none';
+    $('.second-input-container')[0].style.display = 'block';
 }
 
 function selectAge(i) {
-    $('.age-select-btn')[0].innerHTML = i;
+    ageGroup = i;
+    $('.year')[0].value = ageGroup;
 }
-function additionalInfoValidationCheck() {
-    nickname = $('#nickname').val();
-    gender = $('.gender input')[0].checked ? '남성' : ($('.gender input')[1].checked ? '여성' : '');
-    ageGroup = $('.age-select-btn')[0].innerHTML;
+function secondInfoValidationCheck() {
+    phone = $('.phone-number').val();
+    const allAgreeCheck = $('#all-agree')[0].checked;
 
-    if(nickname === '') {
-        alert('닉네임을 입력해주세요.');
+    if (ageGroup === '' || ageGroup === null || ageGroup === undefined) {
+        alert('출생년도를 선택해주세요.');
         return;
     }
-    // if(!nicknameCheck) {
-    //     alert('닉네임 중복체크를 해주세요.');
-    //     return;
-    // }
-    if(gender === '') {
-        alert('성별을 선택해주세요.');
+    if (phone === '' || phone === null) {
+        alert('핸드폰 번호를 입력해주세요.');
         return;
     }
-    if(ageGroup === '출생년도를 선택하세요') {
-        alert('연령대를 선택해주세요.');
+    if (!phoneRegExp.test(phone)) {
+        alert('핸드폰 번호을 지켜주세요. (ex: 010-0000-0000)');
         return;
     }
-    signUpReq(email, password, nickname, gender, ageGroup);
+    if (!allAgreeCheck) {
+        alert('약관에 동의해주세요.');
+        return;
+    }
+    signUpReq(name, gender, email, password, ageGroup, phone);
 }
 
-function signUpReq(email, password, nickname, gender, ageGroup) {
+function signUpReq(name, gender, email, password, ageGroup, phone) {
     spinner = new Spinner(opts).spin(target);
     const info = {
         method: "POST",
@@ -148,7 +141,6 @@ function signUpReq(email, password, nickname, gender, ageGroup) {
         body: {
             email: email,
             password: password,
-            nickname: nickname,
             gender: gender,
             ageGroup: ageGroup,
             social: false
@@ -175,8 +167,8 @@ function signUpReq(email, password, nickname, gender, ageGroup) {
             else if ((e.status === 400 && e.responseText.includes("not provided"))
                 || (e.status === 500 && e.responseText.includes("Cannot read property"))) {
                 console.log("check your parameters");
-            } else if(e.status === 0){
-                if(navigator.onLine){
+            } else if (e.status === 0){
+                if (navigator.onLine){
                     console.log('status : 0');
                 }else {
                     console.log('internet disconnected');
@@ -218,7 +210,7 @@ $(document).ready(() => {
                 console.log('ajax call error: login page - verifyToken');
                 if (e.status === 404 && e.responseText.includes("API call URL not found.")) {
                     console.log("check your URL, method(GET/POST)");
-                }else if(e.status === 403){
+                }else if (e.status === 403){
                     if (e.responseText.includes("No token provided.")) {
                         console.log("No token, no problem.");
                     }
@@ -227,8 +219,8 @@ $(document).ready(() => {
                     else if (e.responseText.includes("invalid signature"))
                         console.log("Modified token");
                     else console.log(e);
-                } else if(e.status === 0){
-                    if(navigator.onLine){
+                } else if (e.status === 0){
+                    if (navigator.onLine){
                         console.log('status : 0');
                     }else {
                         console.log('internet disconnected');
@@ -245,73 +237,92 @@ $(document).ready(() => {
         alert('이미 로그인되어 있습니다.');
         location.href="/chat";
     }
-
-
-    $('.access-terms-chk').click(() => {
-        if($('.terms input')[1].checked) {
-            $('.terms input')[1].checked = false;
-        } else {
-            $('.terms input')[1].checked = true;
-        }
-        autoAgree();
-    })
-    $('.data-terms-chk').click(() => {
-        if($('.terms input')[2].checked) {
-            $('.terms input')[2].checked = false;
-        } else {
-            $('.terms input')[2].checked = true;
-        }
-        autoAgree();
-    })
     // 출생년도 초기화
     for(let i = 2005; i >= 1950; i--) {
-        $('#age-group-list .modal-body').append(`<div class="birth-year" onclick="selectAge(${i})" data-dismiss="modal">${i}</div>`)
+        $('#year-list .modal-body').append(`<div class="birth-year" onclick="selectAge(${i})" data-dismiss="modal">${i}</div>`)
     }
-    // 기본정보 탭 터치
-    firstTab.click(() => {
-        signUpStep = 1;
-        changeStep();
-    });
-    // 부가정보 탭 터치
-    secondTab.click(() => {
-        basicInfoValidationCheck();
-    });
     // 다음 버튼 터치
-    $('.next-btn').click(() => {
-        basicInfoValidationCheck();
+    $('.next-button').click(() => {
+        firstInfoValidationCheck();
     });
     // 뒤로 버튼 터치
     $('.back-btn').click(() => {
-        if(signUpStep === 1) {
-            location.href = '/';
+        if ($('.input-container')[0].style.display === 'none') {
+            $('.input-container')[0].style.display = 'block';
+            $('.second-input-container')[0].style.display = 'none';
+            $('.next-button')[0].style.display = 'block';
+            $('.signup-button')[0].style.display = 'none';
         } else {
-            signUpStep = 1;
-            changeStep();
+            location.href = '/';
         }
     })
-
     // 가입 완료 터치
-    $('.complete-btn').click(() => {
-        additionalInfoValidationCheck();
+    $('.signup-button').click(() => {
+        secondInfoValidationCheck();
     })
     // 패스워드 칠 때 마다 실행
     $('#password').keyup(() => {
-        pwNum = $('#password').val().search(/[0-9]/g);
-        pwEng = $('#password').val().search(/[a-z]/ig);
-        pwSpe = $('#password').val().search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
-
-        if($('#password').val() === '') {
-            $('.password-help')[0].innerHTML = '';
-        }
-        else if($('#password').val().length < 8 && $('#password').val() !== '') {
-            $('.password-help')[0].innerHTML = '비밀번호는 8자리 이상이어야 합니다.';
-            $('.password-help')[0].style.color = 'red';
-        } else if((pwNum < 0 && pwEng < 0) || (pwNum < 0 && pwSpe < 0) || (pwEng < 0 && pwSpe < 0)) {
-            $('.password-help')[0].innerHTML = '비밀번호는 숫자, 영문, 특문 중 2가지 이상을 혼합해야 합니다.';
-            $('.password-help')[0].style.color = 'red';
-        } else {
-            $('.password-help')[0].innerHTML = '적절한 비밀번호입니다.';
-            $('.password-help')[0].style.color = 'green';
-        }
+        // pwNum = $('#password').val().search(/[0-9]/g);
+        // pwEng = $('#password').val().search(/[a-z]/ig);
+        // pwSpe = $('#password').val().search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+        // console.log(pwNum, pwEng, pwSpe);
+        
+        // if ($('#password').val() !== '' && $('#password').val().length >= 8 && !((pwNum < 0 && pwEng < 0) || (pwEng < 0 && pwSpe < 0) || (pwSpe < 0 && pwNum < 0))) {
+        //     $('.password-complete')[0].childNodes[1].style.display = 'block';
+        // } else {
+        //     $('.password-complete')[0].childNodes[1].style.display = 'none';
+        // }
+        // if ($('#password-check').val() !== '') {
+        //     if ($('#password').val() === $('#password-check').val()) {
+        //         $('.password-complete')[1].childNodes[1].style.display = 'block';
+        //     } else {
+        //         $('.password-complete')[1].childNodes[1].style.display = 'none';
+        //     }
+        // }
+        passwordValidationCheck();
     });
+
+    $('#password-check').keyup(() => {
+        // if ($('#password-check').val() !== '') {
+        //     if ($('#password').val() === $('#password-check').val()) {
+        //         $('.password-complete')[1].childNodes[1].style.display = 'block';
+        //     } else {
+        //         $('.password-complete')[1].childNodes[1].style.display = 'none';
+        //     }
+        // }
+        passwordEqualCheck();
+    })
+
+    $('#male').click(() => {
+        if ($('#male')[0].checked) {
+            $('#female')[0].checked = false;
+            gender = 'M';
+        } else {
+            gender = '';
+        }
+    })
+    $('#female').click(() => {
+        if ($('#female')[0].checked) {
+            $('#male')[0].checked = false;
+            gender = 'F';
+        } else {
+            gender = '';
+        }
+    })
+
+    $('#all-agree').click(() => {
+        if ($('#access-terms')[0].checked && $('#privacy-policy')[0].checked) {
+            $('#access-terms')[0].checked = false;
+            $('#privacy-policy')[0].checked = false;
+        } else {
+            $('#access-terms')[0].checked = true;
+            $('#privacy-policy')[0].checked = true;
+        }
+    })
+    $('#access-terms').click(() => {
+        autoAgree();
+    })
+    $('#privacy-policy').click(() => {
+        autoAgree();
+    })
 });
