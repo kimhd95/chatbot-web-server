45faf4aa3ef4e3e0272f1a4c89aef3964e13c182
hyojin1912 <chaosjin1912@gmail.com>
2019-03-07 16:56:44 +0900

    https test

--- foodle/api/api_prototype.js
+++ foodle/api/api_prototype.js
@@ -1,6 +1,6 @@
 Api = function () {
   // this.config = require('../configs');
-  this.hostname = 'http://devbotfood.jellylab.io';
+  this.hostname = 'https://devbotfood.jellylab.io';
   this.request = require('request');
 };
 
