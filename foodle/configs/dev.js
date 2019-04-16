0e5145ef71a6fc3fba5bf2f1e355cf99126e1686
hyojin1912 <chaosjin1912@gmail.com>
2019-04-16 14:12:15 +0900

    운영서버 배포후 개발 서버로 변경

--- foodle/configs/dev.js
+++ foodle/configs/dev.js
@@ -6,10 +6,10 @@ let devConfig = {
 	hostname: 'corgi.jellylab.io',
 	port: 8001,
 	viewDir: './views',
-	//운영서버
-	// api_url: 'https://devapifood.jellylab.io', // Dev API Server on AWS is in http.
 	//개발서버
-	api_url: 'https://foodprod.jellylab.io', // Dev API Server on AWS is in http.
+	api_url: 'https://devapifood.jellylab.io', // Dev API Server on AWS is in http.
+	//운영서버
+	// api_url: 'https://foodprod.jellylab.io', // Dev API Server on AWS is in http.
 	// api_url: 'http://ec2-13-124-160-236.ap-northeast-2.compute.amazonaws.com',
 };
 
