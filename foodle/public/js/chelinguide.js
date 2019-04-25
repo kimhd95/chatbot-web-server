cb100bc146f2b4352d396e9502d4f548415ddda9
hyojin1912 <chaosjin1912@gmail.com>
2019-04-25 14:37:46 +0900

    chelinguide bootstrap link 수정

--- foodle/public/js/chelinguide.js
+++ foodle/public/js/chelinguide.js
@@ -17,28 +17,31 @@ $(document).ready(() => {
     location.href='/lobby';
   })
 
-  socket.on('file number', (msg, index) => {
-    if(index === '1') {
-      for(i=1; i<msg+1; i++){
-        var image_url = `/contents/contents  vegan _페이지_${i}.jpg`;
-        swiper.appendSlide(`<div id="slide_${i}" class="swiper-slide" style="background-image: url('/contents/contents  vegan _페이지_${i}.jpg');"></div>`)
-      }
-    } else if(index === '2') {
-      for(i=1; i<msg+1; i++){
-        var image_url = `/contents/pork cutlet_페이지_${i}.jpg`;
-        swiper.appendSlide(`<div id="slide_${i}" class="swiper-slide" style="background-image: url('/contents/pork cutlet_페이지_${i}.jpg');"></div>`)
-      }
-    }
-  });
+  // socket.on('file number', (msg, index) => {
+  //   if(index === '1') {
+  //     for(i=1; i<msg+1; i++){
+  //       var image_url = `/contents/contents  vegan _페이지_${i}.jpg`;
+  //       swiper.appendSlide(`<div id="slide_${i}" class="swiper-slide" style="background-image: url('/contents/contents  vegan _페이지_${i}.jpg');"></div>`)
+  //     }
+  //   } else if(index === '2') {
+  //     for(i=1; i<msg+1; i++){
+  //       var image_url = `/contents/pork cutlet_페이지_${i}.jpg`;
+  //       swiper.appendSlide(`<div id="slide_${i}" class="swiper-slide" style="background-image: url('/contents/pork cutlet_페이지_${i}.jpg');"></div>`)
+  //     }
+  //   }
+  // });
 
-  $('#order-list').hide();
+
+
+  // $('#order-list').hide();
+
   $('#place-list-button').click(function() {
     console.log('지역선택 버튼 클릭');
 
   });
   $('#order-list-button').click(function() {
     console.log('정렬기준선택 버튼 클릭');
-    // $('#order-list').modal('show');
+    $('#order-list').modal('toggle');
   });
 
   $('#share-function-button, #share-function-icon').click(function() {
