595a75e4132d22325b81c217fcf660ac05c91ad8
hyojin1912 <chaosjin1912@gmail.com>
2019-04-25 18:14:14 +0900

    card header 완성

--- foodle/public/js/chelinguide.js
+++ foodle/public/js/chelinguide.js
@@ -1,6 +1,17 @@
-// var socket = io();
-// // let loginV=sessionStorage.getItem('login');
-//
+let sort, subway;
+function selectSortBy(i) {
+    var order = $('.order input').val();
+    sort = $('#order-list button')[i].innerHTML;
+    $('.order')[0].value = sort;
+    // 정렬해주는 api 필요
+}
+
+function sortBySubway(i) {
+    var order = $('.place-detail input').val();
+    subway = $('#place-list button')[i].innerHTML;
+    $('.place-detail')[0].value = subway;
+    // 정렬해주는 api 필요
+}
 
 $(document).ready(() => {
   var socket = io();
@@ -17,31 +28,12 @@ $(document).ready(() => {
     location.href='/lobby';
   })
 
-  // socket.on('file number', (msg, index) => {
-  //   if(index === '1') {
-  //     for(i=1; i<msg+1; i++){
-  //       var image_url = `/contents/contents  vegan _페이지_${i}.jpg`;
-  //       swiper.appendSlide(`<div id="slide_${i}" class="swiper-slide" style="background-image: url('/contents/contents  vegan _페이지_${i}.jpg');"></div>`)
-  //     }
-  //   } else if(index === '2') {
-  //     for(i=1; i<msg+1; i++){
-  //       var image_url = `/contents/pork cutlet_페이지_${i}.jpg`;
-  //       swiper.appendSlide(`<div id="slide_${i}" class="swiper-slide" style="background-image: url('/contents/pork cutlet_페이지_${i}.jpg');"></div>`)
-  //     }
-  //   }
-  // });
-
-
-
-  // $('#order-list').hide();
-
   $('#place-list-button').click(function() {
     console.log('지역선택 버튼 클릭');
 
   });
   $('#order-list-button').click(function() {
     console.log('정렬기준선택 버튼 클릭');
-    $('#order-list').modal('toggle');
   });
 
   $('#share-function-button, #share-function-icon').click(function() {
