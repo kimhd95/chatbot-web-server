50185673e0a5b118b94a881edf56d48936ace34f
Kim Hyun Dong <kimhd10@naver.com>
2019-05-23 17:18:19 +0900

    구글 자동로그인방지 오류수정 & ejs script이동

--- foodle/public/js/index.js
+++ foodle/public/js/index.js
@@ -9,7 +9,7 @@ var pwSpe;
 // // 구글 로그인
 function onSignIn(googleUser) {
     // 구글로그인 눌렀을 때만 실행되도록 (googleLoginReq은 버튼클릭이벤트에서 초기화)
-    if (!sessionStorage.getItem('loginPlatform') == 'google') {
+    if (sessionStorage.getItem('loginPlatform') != 'google') {
       console.log("구글 자동로그인 방지");
       return;
     }
@@ -51,8 +51,111 @@ function onSignIn(googleUser) {
         }
         sendTokenReq(info);
     }
-
 }
+
+// 페북로그인
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback');
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected' && sessionStorage.getItem('loginPlatform') == 'facebook') {
+    // Logged into your app and Facebook.
+    testAPI();
+  }
+}
+
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
+  sessionStorage.setItem('loginPlatform', 'facebook');
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '577461736068907',
+    cookie     : true,  // enable cookies to allow the server to access
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v3.2' // The Graph API version to use for the call
+  });
+
+  // Now that we've initialized the JavaScript SDK, we call
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  // FB.getLoginStatus(function(response) {
+  //   console.log("aaa");
+  //   statusChangeCallback(response);
+  // });
+
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', {fields: ['email', 'name']}, function(response) {
+    console.log(JSON.stringify(response));
+
+    const info = {
+        url: '/api/v1/users/social_login',
+        method: 'POST',
+        body: {
+            email: response.email,
+            name: response.name,
+            token: 'facebook'
+        },
+        success: function (res) {
+            if (res.success) {
+              sessionStorage.setItem('email', response.email);
+              sessionStorage.setItem('name', response.name);
+              sessionStorage.setItem('login', '3');
+              window.location.replace(res.redirect);
+            } else {
+              console.log(res);
+            }
+        },
+        error: function (e) {
+            if (e.responseJSON.message === 'This email is Already signed up.') {
+                localStorage.clear();
+                sessionStorage.clear();
+                alert('이미 가입된 이메일입니다.');
+            } else {
+                alert('접근 불가능합니다.');
+            }
+        }
+    }
+    sendTokenReq(info);
+  });
+}
+
+
+
+
 // 이메일 로그인 validation check
 function loginValidationCheck() {
     email = $('.email').val();
